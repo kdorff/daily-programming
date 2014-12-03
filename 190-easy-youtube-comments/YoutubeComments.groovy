813611bbfdce4afbd847a5be0e8b46cf624d3bd2
Kevin Dorff <kevin@dorffweb.com>
2014-12-03 14:29:26 -0600

    cleanup

--- 190-easy-youtube-comments/YoutubeComments.groovy
+++ 190-easy-youtube-comments/YoutubeComments.groovy
@@ -1,5 +1,78 @@
+/**
+ * http://www.reddit.com/r/dailyprogrammer/comments/2nauiv/20141124_challenge_190_easy_webscraping_sentiments/
+ * 
+ * Description
+ * --------------------
+ * Webscraping is the delicate process of gathering information from a website 
+ * (usually) without the assistance of an API. Without an API, it often involves 
+ * finding what ID or CLASS a certain HTML element has and then targeting it. 
+ * In our latest challenge, we'll need to do this (you're free to use an API, 
+ *     but, where's the fun in that!?) to find out the overall sentiment of a 
+ * sample size of people.
+ * We will be performing very basic sentiment analysis on a YouTube video of 
+ * your choosing.
+ * 
+ * Task
+ * --------------------
+ * Your task is to scrape N (You decide but generally, the higher the sample, 
+ *     the more accurate) number of comments from a YouTube video of your 
+ * choice and then analyse their sentiments based on a short list of happy/sad 
+ * keywords.
+ * 
+ * Analysis will be done by seeing how many Happy/Sad keywords are in each 
+ * comment. If a comment contains more sad keywords than happy, then it can 
+ * be deemed sad.
+ * 
+ * Here's a basic list of keywords for you to test against. I've ommited 
+ * expletives to please all readers...
+ * 
+ * happy = ['love','loved','like','liked','awesome','amazing',
+ *          'good','great','excellent']
+ * sad =   ['hate','hated','dislike','disliked','awful','terrible',
+ *          'bad','painful','worst']
+ * 
+ * Feel free to share a bigger list of keywords if you find one. A larger one 
+ * would be much appreciated if you can find one.
+ * 
+ * Formal inputs and outputs
+ * --------------------
+ * 
+ * Input description
+ * --------------------
+ * On console input, you should pass the URL of your video to be analysed.
+ * 
+ * Output description
+ * --------------------
+ * The output should consist of a statement stating something along the 
+ * lines of -
+ * 
+ * "From a sample size of" N "Persons. This sentence is mostly" [Happy|Sad] 
+ * "It contained" X "amount of Happy keywords and" X "amount of sad keywords. 
+ * The general feelings towards this video were" [Happy|Sad]
+ * 
+ * Notes
+ * --------------------
+ * As pointed out by /u/pshatmsft , YouTube loads the comments via AJAX so 
+ * there's a slight workaround that's been posted by /u/threeifbywhiskey .
+ * 
+ * Given the URL below, all you need to do is replace FullYoutubePathHere with 
+ * your URL
+ * 
+ * https://plus.googleapis.com/u/0/_/widget/render/comments?first_party_property=YOUTUBE&href=FullYoutubePathHere
+ * 
+ * Remember to append your url in full 
+ * (https://www.youtube.com/watch?v=dQw4w9WgXcQ as an example)
+ * 
+ * Hints
+ * --------------------
+ * The string for a Youtube comment is the following
+ * <div class="CT">Youtube comment here</div>
+ */
+
 // REST URL required to fetch comments for a specific video
-commentsBaseUrl = "https://plus.googleapis.com/u/0/_/widget/render/comments?first_party_property=YOUTUBE&href=_URL_"
+commentsBaseUrl = "https://plus.googleapis.com/u/0/_/widget/render/comments" +
+                  "?first_party_property=YOUTUBE&href=_URL_"
+
 // Videos to check comments for
 inputs = [
     'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
@@ -31,21 +104,23 @@ inputs.each { checkUrl ->
     def data = new URL(url).text.toLowerCase()
 
     def allFeelings = [positives: 0, negatives: 0]
+    int commentsCount = 0
     data.findAll(commentPattern) { whole, comment ->
+        commentsCount++
         parseComment(comment, allFeelings)
     }
 
     // Convert allFeelings to a human readable string and output
     def overallFeelings = feelingsToHuman(allFeelings)
-    println "Comments for ${checkUrl} are overall ${overallFeelings}"
-    println allFeelings
+    print   "${commentsCount} comments for ${checkUrl} are "
+    println "overall ${overallFeelings}"
 }
 
 /**
  * Given a single comment HTML string, determine if it is positive
  * or negative.
  * @param comment comment in HTML format
- * @param map of feelings across all comments with int values for
+ * @param feelings a map of feelings across all comments with int values for
  * keys positives, negatives which are the number of positive and
  * negative words found in the video.
  */
@@ -63,6 +138,12 @@ def parseComment(comment, feelings) {
     feelings
 }
 
+/**
+ * Convert the feelings map to a string describing the feelings.
+ * @param feelings a map of feelings across all comments with int values for
+ * keys positives, negatives which are the number of positive and
+ * negative words found in the video.
+ */
 def feelingsToHuman(feelings) {
     int delta = feelings.positives - feelings.negatives
     if (delta == 0) {
