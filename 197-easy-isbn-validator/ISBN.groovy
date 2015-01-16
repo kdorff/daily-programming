481ba5dd875eb68f483f4ff91717ef39343969f6
Kevin Dorff <kevin@dorffweb.com>
2015-01-16 16:29:53 -0600

    comments, improvements

--- 197-easy-isbn-validator/ISBN.groovy
+++ 197-easy-isbn-validator/ISBN.groovy
@@ -1,21 +1,61 @@
+/**
+ * http://www.reddit.com/r/dailyprogrammer/comments/2s7ezp/20150112_challenge_197_easy_isbn_validator/
+ * Code to validate ISBN numbers.
+ *
+ * Author: Kevin Dorff
+ */
+
+/**
+ * This is the default class for this script. Test validating
+ * an ISBN.
+ */
 class ISBN extends GroovyTestCase {
     void testCalc() {
-        assert true == IsbnValidator.validate("0-7475-3269-9")
+        assert true == ISBNValidator.validate("0-7475-3269-9")
+    }
+
+    void testCalcFailWrongCheck() {
+        assert false == ISBNValidator.validate("0-7475-3269-X")
+    }
+
+    void testCalcWrongLength() {
+        assert false == ISBNValidator.validate("0-7475-3269")
+    }
+
+    void testCalcBadDigit() {
+        assert false == ISBNValidator.validate("0-7475-326X-9")
     }
 }
 
-class IsbnValidator {
-    def static digitPattern = ~/([0-9X])/
+/**
+ * Class to validate 10 digit ISBN numbers such as
+ * "0-7475-3269-9".
+ */
+class ISBNValidator {
+    // ISBN must match this pattern
+    def static isbnPattern = 
+        ~/^(\d)-(\d)(\d)(\d)(\d)-(\d)(\d)(\d)(\d)-(\d|X)$/
+
+    /**
+     * Validate a ten digit ISBN value.
+     * @param isbn the isbn to validate
+     * @return true if the ISBN validates, otherwise false
+     */
     def static validate(String isbn) {
         def validates = false
-        def finds = isbn.findAll(digitPattern).findAll().collect {
-            (it == 'X') ? 10 : (it as Integer)
-        }
-        int sum = 0
-        if (finds.size() == 10) {
+        def matcher =  isbn =~ isbnPattern
+        if (matcher.matches()) {
+            // matcher[0][0] is the whole match, the
+            // rest of the elements of matcher[0][n]
+            // are the capture groups, so we will
+            // skip matcher[0][0] in the following
+            def digits = matcher[0][1..-1].collect {
+                it == 'X' ? 10 : it as Integer
+            }
+            int sum = 0
             int multiplier = 10
-            (0 .. finds.size()).each { v ->
-                sum += multiplier-- * v
+            digits.eachWithIndex { v, i ->
+                sum += (10 - i) * v
             }
             validates = sum % 11 == 0
         }
--- 197-easy-isbn-validator/README.md
+++ 197-easy-isbn-validator/README.md
@@ -1,3 +1,7 @@
+# Implementation
+
+Challenge from: http://www.reddit.com/r/dailyprogrammer/comments/2s7ezp/20150112_challenge_197_easy_isbn_validator/
+
 # Description
 
 ISBN's (International Standard Book Numbers) are identifiers for 
