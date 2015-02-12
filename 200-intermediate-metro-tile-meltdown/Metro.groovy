9308a984abbc0fce1e0b4f6757f94e10ff95f2c2
Kevin Dorff <kevin@dorffweb.com>
2015-02-12 17:04:43 -0600

    Optimizations

--- 200-intermediate-metro-tile-meltdown/Metro.groovy
+++ 200-intermediate-metro-tile-meltdown/Metro.groovy
@@ -1,7 +1,19 @@
+/**
+ * This implementation requires each tile be bordered by
+ * ".", which is part of the problem description. It will
+ * NOT currently handle tiles that are all the way to the
+ * edges, such as the example:
+ *
+ *    4 4
+ *    xx.z
+ *    xx..
+ *    ..yy
+ *    z.yy
+ */
 class Metro {
 
     /** Input file */
-    def inputFile = "grid1.txt"
+    def inputFile = "grid3.txt"
 
     /** Storage for the grid. One row per list entry. */
     List<String> grid
@@ -10,7 +22,9 @@ class Metro {
     /** height of grid. */
     int height
     /** The found tiles. */
-    Tiles tiles
+    List<Tile> tiles
+    /** Regex pattern for finding the sub-blocks in a line */
+    def blockStartPattern = ~/(?<=\.)([^.]+)/
 
     /**
      * Program start point.
@@ -24,7 +38,7 @@ class Metro {
      */
     public Metro() {
         grid = []
-        tiles = new Tiles()
+        tiles = []
     }
 
     /**
@@ -36,8 +50,11 @@ class Metro {
         output()
     }
 
+    /**
+     * Output the final results.
+     */
     def output() {
-        tiles.tiles.each { Tile tile ->
+        tiles.each { Tile tile ->
             println "${tile.width}×${tile.height} tile of " + 
                 "character '${tile.tileLetter}' " +
                 "located at (${tile.c},${tile.r})"
@@ -52,10 +69,12 @@ class Metro {
      * @param inputFile the input file containing the grid
      */
     def readGrid(String inputFile) {
+        grid.clear()
         int i = 0  // Line number in file
         new File(inputFile).eachLine { line ->
             if (line.startsWith("#")) {
                 // Comment. Ignore.
+                println line
             } else if (i == 0) {
                 // Width and height of the grid
                 (width, height) = line.split(' ').collect { it as Integer }
@@ -71,14 +90,16 @@ class Metro {
      * Process the grid, finding the sub-blocks.
      */
     def processGrid() {
-        tiles = new Tiles()
+        tiles.clear()
         (0 ..< height).each { r ->
-            int c = 0
-            grid[r].split('[.]').each { part ->
-                if (part && !tiles.inTiles(r, c)) {
-                    consume(r, c)
+            def matcher = blockStartPattern.matcher(grid[r])
+            while(matcher.find()) {
+                // Find the start of each block on this row
+                int c = matcher.start()
+                if ((tiles.find { it.inTile(r, c) }) == null) {
+                    int width = matcher.end() - matcher.start()
+                    consumeTile(r, c, width)
                 }
-                c += part.size() + 1
             }
         }
     }
@@ -87,39 +108,21 @@ class Metro {
      * Consume the sub-block finding width and height. It's then marked
      * as '.' (gutter, background) so it won't be processed again.
      */
-    def consume(int startR, int startC) {
-        // Find the width
-        int r = startR
-        int c = startC
-        def width = 0
-        def blockLetter
-        // Find the width of the sub-block
-        while (true) {
-            def letter = grid[r][c + width]
-            if (letter == '.') {
-                break
-            }
-            if (blockLetter == null) {
-                // Define the letter for this grid, the top-left letter
-                blockLetter = letter
-            }
-            width++
-            letter = grid[r][c + width]
-        }
-
+    def consumeTile(int r, int c, int width) {
         // Find the height of the sub-block
-        r = startR
-        c = startC
+        def letter = grid[r][c]
+        def tileLetter = letter
+
         def height = 0
-        while (true) {
-            def letter = grid[r + height][c]
-            if (letter == '.') {
-                break
-            }
+        while (letter != '.') {
             height++
+            letter = grid[r + height][c]
         }
 
-        tiles.addTile new Tile(blockLetter, r, c, width, height)
+        tiles << new Tile(
+            tileLetter, 
+            new IntRange(true, r, r + height - 1),
+            new IntRange(true, c, c + width - 1))
     }
 }
 
@@ -127,37 +130,23 @@ class Metro {
  * A found tile
  */
 class Tile {
+    /** The letter for this tile. */
+    String tileLetter
     /** Inclusive range for the columns for this tile. */
     IntRange colRange
     /** Inclusive range for the rows for this tile. */
     IntRange rowRange
-    /** The top-left row for this tile. */
-    int r
-    /** The top-left column for this tile. */
-    int c
-    /** The width for this tile. */
-    int width
-    /** The height for this tile. */
-    int height
-    /** The letter for this tile. */
-    String tileLetter
 
     /**
      * Construct a new Tile.
      * @param tileLetter the letter of this tile
-     * @param r the top-left row of this tile
-     * @param c the top-left column of this tile
-     * @param width the width of this tile
-     * @param height the height of this tile
+     * @param colRange the inclusive IntRange for columns covered by this ile
+     * @param rowRange the inclusive IntRange for rows covered by this tile
      */
-    public Tile(String tileLetter, int r, int c, int width, int height) {
-        colRange = new IntRange(true, c, c + width - 1)
-        rowRange = new IntRange(true, r, r + height - 1)
+    public Tile(String tileLetter, IntRange rowRange, IntRange colRange) {
+        this.rowRange = rowRange
+        this.colRange = colRange
         this.tileLetter = tileLetter
-        this.r = r
-        this.c = c
-        this.width = width
-        this.height = height
     }
 
     /**
@@ -169,36 +158,16 @@ class Tile {
     def inTile(int r, int c) {
         colRange.contains(c) && rowRange.contains(r) 
     }
-}
 
-/**
- * Store the tiles found.
- */
-class Tiles {
-    /** The found files */
-    List<Tile> tiles = []
-    /**
-     * Add a found tile.
-     * @param tile the newly found tile
-     */
-    def addTile(Tile tile) {
-        tiles << tile
-    }
+    /** Get the top-left column for this tile */
+    def getC() { colRange.getFromInt() }
 
-    /**
-     * Determine if (r,c) is already part of a found tile.
-     * @param r row
-     * @param c col
-     * @param true if (r,c) is in an already found tile
-     */
-    def inTiles(int r, int c) {
-        def found = false
-        for (Tile tile in tiles) {
-            if (tile.inTile(r, c)) {
-                found = true
-                break
-            }
-        }
-        found
-    }
+    /** Get the top-left row for this tile */
+    def getR() { rowRange.getFromInt() }
+
+    /** Get the width for this tile */
+    def getWidth()  { colRange.getToInt() - colRange.getFromInt() + 1 }
+    
+    /** Get the height for this tile */
+    def getHeight() { rowRange.getToInt() - rowRange.getFromInt() + 1 }
 }
